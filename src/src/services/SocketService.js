bd1089685a087a7bc65e69460f00a6622a5d5060
jny <ny030303@naver.com>
2021-09-07 16:55:19 +0900

    clientLink 수정

--- src/src/services/SocketService.js
+++ src/src/services/SocketService.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 export const serverLink = "http://localhost:54000/"; //  "http://1.238.234.196:54000/";
-export const clientLink = "http://localhost:3000/";
+export const clientLink = window.location.origin+"/";
 export const socket = io(serverLink, {
   withCredentials: true,
   extraHeaders: {
